58abd93c7b4d033534466516108a4a7a8ed569ce
nanikoduri <nani15@stu.calumsva.edu>
2017-09-20 13:12:12 -0400

    new program added

---
+++ src/nani/second.java
@@ -0,0 +1,13 @@
+package nani;
+
+public class second {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("hi nani");
+	}
+
+}
